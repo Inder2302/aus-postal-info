00e3747b138eda640aed51b23b851be47e224930
Indrajeet Kalra <Inder@Indrajeets-MacBook-Pro.local>
2020-09-15 15:16:32 +1000

    Initial commit... barebones maven project from spring starter

---
+++ src/main/java/com/ijkalra/auspostalinfo/AusPostalInfoApplication.java
@@ -0,0 +1,13 @@
+package com.ijkalra.auspostalinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusPostalInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AusPostalInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijkalra/auspostalinfo/AusPostalInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijkalra.auspostalinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusPostalInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
